9cd875748e124a97c9be2b16cfd8004fe9e5bc70
Flávio Eduardo <xflax00@gmail.com>
2023-05-01 19:48:37 -0300

    route /

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 // Router
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
 
 // Pages
 import Home from './pages/Home/Home'
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/repository-finder" />} />
       <Route path="/repository-finder" element={<Home />} />
       <Route path="/repository-finder/:user" element={<Home />} />
       <Route path="/repository-finder/repos/:user" element={<Repos />} />
